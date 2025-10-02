81cc7d1b01750db5421d3e6a23c6f0186afa1248
Luis Martinez <Luisricardo494@gmail.com>
2025-10-01 21:03:19 -0500

    added home page layout and public assets for project (#2)
    Co-authored-by: Luis <beebs@Luiss-MacBook-Pro.local>

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <BookOverview />
+      <BookList />
+    </div>
+  );
+};
+
+export default Home;
